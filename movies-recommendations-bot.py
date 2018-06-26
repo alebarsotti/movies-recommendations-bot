1f64094a4404b4d523a2d68fad90f8f02c01635b
Alejandro Barsotti <alejandro.a.barsotti@gmail.com>
2018-06-25 22:06:43 -0300

    Bot v1.

--- movies-recommendations-bot.py
+++ movies-recommendations-bot.py
@@ -1,10 +1,46 @@
 import tweepy
+import pandas as pd
 import os
 
+print('Running in Heroku!')
+
+CSV_URL = os.environ.get('CSV_URL', None)
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY', None)
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', None)
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', None)
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET', None)
+IMDB_URL = os.environ.get('IMDB_URL', None)
+
+def init_dataset():
+    dataset = pd.read_csv(CSV_URL)
+    dataset.set_index('imdb_id', inplace=True)
+    return dataset
+
+def get_movie_recommendation(username):
+    recommendation = dataset.sample(1)
+    for row in recommendation.itertuples():
+        tweet = f'Hola, {username}! Te recomiendo: \n'
+        tweet += f'{row.title} ({row.year}) [{row.spanish_title}] \n'
+        tweet += f'{row.genres} \n' 
+        tweet += f'Puntuación: {row.rating} ({row.votes} votos) \n'
+        tweet += f'{IMDB_URL}{row.Index}'
+        return tweet
+
+def process_status(status):
+    # Verify that the status isn't a reply nor a quote.
+    if status.in_reply_to_status_id or status.is_quote_status:
+        return
+
+    username = f'@{status.author.screen_name}'
+    tweet_id = status.id
+    recommendation = get_movie_recommendation(username)
+
+    api.update_status(recommendation, tweet_id)
+
+    #hashtags = [x['text'] for x in status.entities['hashtags']]
+    #print('Hashtags:', hashtags)    
+    #print('Name:', status.author.name)
+    #print('Text:', status.text)
 
 def init_tweepy_api():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -12,5 +48,14 @@ def init_tweepy_api():
     api = tweepy.API(auth)
     return api
 
+class MentionsStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        process_status(status)
+
+dataset = init_dataset()
 api = init_tweepy_api()
-api.update_status('Hello World from Heroku!')
+
+mentionsStreamListener = MentionsStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=mentionsStreamListener)
+myStream.filter(track=['@MoviesMasterBot'])
